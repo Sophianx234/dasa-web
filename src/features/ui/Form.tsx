d3694ba56b84f5f395011ef4d7a73ef481b3bb0f
Damian X <dx4336969@gmail.com>
2024-11-10 18:46:01 +0000

    implementing login section

--- src/features/ui/Form.tsx
+++ src/features/ui/Form.tsx
@@ -6,6 +6,7 @@ import { PiBuildingApartmentLight } from "react-icons/pi"
 import { useAppSelector } from "../utils/hooks"
 import Select from "./Select"
 import SVGLite from "./SVGLite"
+import { LuContact2 } from "react-icons/lu"
 
 function Form() {
     const isAnnex = useAppSelector(store=>store.user.isAnnex)
@@ -15,8 +16,8 @@ function Form() {
         <div className="bg-dasalight h-dvh pt-6 ">
             <div className="px-4">
                 <div>
-                    <h1 className="text-[#33312e] font-bold text-3xl ">Let's get started</h1>
-                    <p className=" font-mulish text-sm">Create your DaSA account. Already <br /> registered? <Link to='login' className="text-blue-950 font-bold">Login here</Link></p>
+                    <h1 className="text-[#33312e] font-bold pb-2 text-3xl ">Let's get started</h1>
+                    <p className=" font-mulish text-sm pb-4">Create your DaSA account. Already <br /> registered? <Link to='/policy' className="text-blue-950 font-bold hover:underline italic">Login here</Link></p>
                 </div>
 
                 <form action="" className="grid grid-cols-2 gap-x-5 gap-y-3">
@@ -31,15 +32,19 @@ function Form() {
 
                     <FormInput type="email" addClass="col-span-2" style="bg-transparent border-b-2 border-b-dasadeep focus:outline-none " placeholder="Enter your email address" icon={<IoMailOutline className="absolute left-2  "/>}/>
 
-                    <FormInput type="select" placeholder="Hall of Residence " style="bg-transparent" icon={<PiBuildingApartmentLight className="absolute left-2   "/>}/>
+                    <FormInput type="select" placeholder="Hall of Residence " style="bg-transparent border-b-2 border-b-dasadeep text-xs" icon={<PiBuildingApartmentLight className="absolute left-2   "/>}/>
                     
 
-                    {isAnnex === 'Annex' || isAnnex === 'UGEL Hostel'?<Select type="select" style="bg-transparent" placeholder={`${isAnnex}`} icon={<PiBuildingApartmentLight className="absolute left-2   "/>}/>: null}
+                    {isAnnex === 'Annex' || isAnnex === 'UGEL Hostel'?<Select type="select" style="bg-transparent border-b-2 border-b-dasadeep" placeholder={`${isAnnex}`} icon={<PiBuildingApartmentLight className="absolute left-2   "/>}/>: null}
 
                     <Select 
-                    style="bg-transparent" 
+                    style="bg-transparent border-b-2 border-b-dasadeep text-sm " 
                     type="select" 
                    form="course" placeholder={`Course`} icon={<PiBuildingApartmentLight className="absolute left-2   "/>}/>
+                    <FormInput 
+                    style="bg-transparent border-b-2 border-b-dasadeep focus:outline-none indent-2 " type="tel" placeholder="Contact " addClass="col-span-2"
+                     icon={<LuContact2
+                    className="absolute left-2   "/>}/>
                     <FormInput 
                     style="bg-transparent border-b-2 border-b-dasadeep focus:outline-none indent-2 " type="password" placeholder="Password " addClass="col-span-2"
                      icon={<IoLockClosedOutline
@@ -49,8 +54,13 @@ function Form() {
                     style="bg-transparent border-b-2 border-b-dasadeep focus:outline-none  indent-2 " addClass="col-span-2"
                     placeholder="Confirm Password " icon={<IoLockOpenOutline className="absolute left-2   "/>}/>
                 </form>
-                <p>By signing up, you acknowledge that you’ve read and accepted our Terms of Service and Privacy Policy.</p>
-                <button className="bg-dasadeep" >Signup</button>
+                <p className="pt-7 pb-5">By signing up, you acknowledge that you’ve read and accepted our <Link to='/terms' className="text-blue-950 font-bold hover:underline italic"> Terms of Service</Link> and <Link to='/policy' className="text-blue-950 font-bold hover:underline italic">
+                   Privacy Policy.
+                 </Link></p>
+                <div className="text-center pb-6">
+
+                <button className="w-full py-2 font-Montserrat font-bold  bg-dasadeep text-[#33312e] hover:bg-transparent border-2 border-transparent hover:border-dasadeep rounded-lg duration-150 transition-all" >Signup</button>
+                </div>
             </div>
             <SVGLite type="sticks"/>
         </div>
