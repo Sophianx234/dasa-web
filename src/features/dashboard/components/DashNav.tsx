c93a25c48320a8c876d5a40e42c4762ac748e9ca
Damian X <dx4336969@gmail.com>
2024-11-14 08:23:20 +0000

    implementing dashnav

--- src/features/dashboard/components/DashNav.tsx
+++ src/features/dashboard/components/DashNav.tsx
@@ -10,7 +10,12 @@ import { FaRegUser } from "react-icons/fa6";
 
 function DashNav() {
     return (
-        <div className="fixed -top-1 bottom-0 bg-white  left-0 right-0  z-40 pt-8 flex flex-col justify-between pb-5 px-4">
+        <div className="fixed -top-1 bottom-0 bg-white  left-0 right-0  z-40 pt-8  pb-5 px-4">
+            <div>
+                <img src="https://i.ibb.co/n8hRM6d/dasalogo-removebg.png" alt="" className="w-16"/>
+            </div>
+            <div className="flex flex-col justify-between h-full">
+
             <ul className="space-y-4  ">
                 <li className="flex  items-center gap-3 border py-4 px-2 font-Montserrat text-xl group hover:bg-dasalight hover:py-4 hover:px-4 rounded-xl transition-all duration-150 font-bold text-[#191817] ">
                 <RiHome3Line className="size-8"/><Link to='dashboard/overview' className="flex justify-between  items-center w-full"><span>Home</span>  <span className="hidden group-hover:block"><IoIosArrowForward /></span>
@@ -31,14 +36,15 @@ function DashNav() {
                 <FaRegUser className="size-8"/><Link to='dashboard/overview' className="w-full flex justify-between items-center">Account<span className="hidden group-hover:block"><IoIosArrowForward/></span>
                 </Link></li>
             </ul>
-            <div className="flex items-center">
+            <div className="flex items-center space-x-2">
                 <AvatarComponent/>
-                <div>
+                <div className="">
                     <h1 className="font-bold font-mulish text-sm">Damian</h1>
                     <p className="text-xs">@xlr8</p>
                 </div>
             </div>
         </div>
+            </div>
     )
 }
 
