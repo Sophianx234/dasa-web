3471b7b3c240a0000ab7c582dd8c27cf0237e490
Damian X <dx4336969@gmail.com>
2024-11-17 08:44:30 +0000

    implementing payment section

--- src/features/dashboard/components/PlanTypeItem.tsx
+++ src/features/dashboard/components/PlanTypeItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { GiReceiveMoney } from "react-icons/gi"
 import { TbBusinessplan } from "react-icons/tb"
 export type planTypeItemProps = {
@@ -8,27 +9,32 @@ export type planTypeItemProps = {
 
 }
 function PlanTypeItem({planType,planPackage,price,planDesc}:planTypeItemProps) {
+    const [toggleRadio, setToggleRadio] = useState(false)
+    function handleSelection(e){
+        setToggleRadio(toggle=>!toggle)
+    }
     return (
         <div>
             <div className="">
                 <div className=" border px-4 rounded-lg
                 pb-2
                 ">
-                    <div className="grid grid-cols-[1fr_1fr_2.5fr] items-center pt-2 pb-2 ">
+                    <div className="grid grid-cols-[1fr_1fr_1fr] items-center pt-2 pb-2 ">
                     <div className="flex items-center gap-3 ">
 
                     
-                        <input type="radio" name="" id="" />
+                       <div onClick={handleSelection} className={`border-8  size-6 ${toggleRadio&&'border-dasadeep'} rounded-full`}>
+
+                       </div>
                     <h1 className="font-bold font-mulish">{planType}</h1>
                     </div>
                     <div className="flex items-center gap-1 border rounded-full mx-2 px-2 ">
-                    <GiReceiveMoney className="fill-dasadeep"/> <p>{planPackage} </p>
+                    <GiReceiveMoney className="fill-dasadeep size-8"/> <p>{planPackage} </p>
                     </div>
                     <p className="justify-self-end text-dasadeep font-mulish font-bold"> ₵{price}/sem</p>
                     </div>
                     <div className="text-smfont-poppins  grid grid-cols-[.2fr_2fr] ">
-                        <div></div>
-                    <p className="text-sm font-mulish  ">{planDesc}</p>
+                    <p className="text-sm font-mulish font-medium col-span-2  ">{planDesc}</p>
                     </div>
                 </div>
             </div>
