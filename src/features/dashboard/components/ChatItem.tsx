32034b034f6d16926d02ae5a57e5b4e4b4828a5c
Damian X <dx4336969@gmail.com>
2024-11-19 11:24:40 +0000

    implementing anonymous messaging

--- src/features/dashboard/components/ChatItem.tsx
+++ src/features/dashboard/components/ChatItem.tsx
@@ -1,8 +1,16 @@
-function ChatItem() {
+export type chatItemProps = {
+    chat: string,
+    orient?:'reverse'
+}
+function ChatItem({chat,orient}:chatItemProps) {
     return (
-        <div>
-            
-        </div>
+        <div className={`  flex  items-center  ${orient === 'reverse'? 'flex-row-reverse ':'flex-row'} mx-2` }>
+<div className="rounded-full mt-16 gap-2 bg-[#fffaf6] border-2 border-[#66615c] mr-2">
+
+<img src='https://i.ibb.co/2SJsCBB/ano-2.jpg' className=" self-end fill-black rounded-full w-20"/>
+</div>
+                <div className={`border px-4 mr-4 py-4  font-mulish rounded-3xl font-semibold  ${orient==='reverse'?'bg-orange-100 text-[#191815] rounded-br-none':'bg-[#fffaf6] text-[#191919] rounded-bl-none '}  border-[#66615c] text-sm`}>{chat} </div>
+                </div> 
     )
 }
 
