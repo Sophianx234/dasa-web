a6516e49471c061c1f06345c32b12bf8bda68c6a
Damian X <dx4336969@gmail.com>
2024-11-19 14:33:30 +0000

    fixing anonymous section bug

--- src/features/dashboard/components/ChatItem.tsx
+++ src/features/dashboard/components/ChatItem.tsx
@@ -1,16 +1,14 @@
 export type chatItemProps = {
     chat: string,
-    orient?:'reverse'
+    orient?:string |boolean
 }
 function ChatItem({chat,orient}:chatItemProps) {
     return (
-        <div className={`  flex  items-center  ${orient === 'reverse'? 'flex-row-reverse ':'flex-row'} mx-2` }>
-<div className="rounded-full mt-16 gap-2 bg-[#fffaf6] border-2 border-[#66615c] mr-2">
-
-<img src='https://i.ibb.co/2SJsCBB/ano-2.jpg' className=" self-end fill-black rounded-full w-20"/>
-</div>
-                <div className={`border px-4 mr-4 py-4  font-mulish rounded-3xl font-semibold  ${orient==='reverse'?'bg-orange-100 text-[#191815] rounded-br-none':'bg-[#fffaf6] text-[#191919] rounded-bl-none '}  border-[#66615c] text-sm`}>{chat} </div>
-                </div> 
+        <div className="flex items-start">
+        <div className="bg-blue-500 text-white p-3 rounded-lg max-w-xs shadow-md">
+          {chat}
+        </div>
+      </div>
     )
 }
 
