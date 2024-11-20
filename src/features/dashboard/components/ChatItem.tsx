11c0d2795f4149d80ac86d5826e05e808c7e6851
Damian X <dx4336969@gmail.com>
2024-11-20 07:05:31 +0000

    implementing speech bubble

--- src/features/dashboard/components/ChatItem.tsx
+++ src/features/dashboard/components/ChatItem.tsx
@@ -3,24 +3,31 @@ export type chatItemProps = {
     orient?:string |boolean
 }
 function ChatItem({chat,orient}:chatItemProps) {
+    if(orient !=='reverse')
     return (
         <>
-        <div className="chat chat-start">
-  <div className="chat-image avatar">
-    <div className="w-10 rounded-full">
-      <img
-        alt="Tailwind CSS chat bubble component"
-        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-    </div>
-  </div>
-  <div className="chat-header">
-    Obi-Wan Kenobi
-    <time className="text-xs opacity-50">12:45</time>
-  </div>
-  <div className="chat-bubble">You were the Chosen One!</div>
-  <div className="chat-footer opacity-50">Delivered</div>
-</div>
-<div className="chat chat-end">
+       
+          <div className="chat chat-start">
+            
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Obi-Wan Kenobi
+          <time className="text-xs opacity-50">12:45</time>
+        </div>
+        <div className="chat-bubble">{chat}</div>
+        <div className="chat-footer opacity-50">Delivered</div>
+      </div>
+      </>
+)
+else return (
+<>
+    <div className="chat chat-end">
   <div className="chat-image avatar">
     <div className="w-10 rounded-full">
       <img
@@ -32,7 +39,7 @@ function ChatItem({chat,orient}:chatItemProps) {
     Anakin
     <time className="text-xs opacity-50">12:46</time>
   </div>
-  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-bubble">{chat}</div>
   <div className="chat-footer opacity-50">Seen at 12:46</div>
 </div>
         </>
