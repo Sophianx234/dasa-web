cb452c83dd9520e0f8864732b9a410a9bc2667b2
Damian X <dx4336969@gmail.com>
2024-11-20 06:48:52 +0000

    implementing chat bubble

--- src/features/dashboard/components/ChatItem.tsx
+++ src/features/dashboard/components/ChatItem.tsx
@@ -4,11 +4,38 @@ export type chatItemProps = {
 }
 function ChatItem({chat,orient}:chatItemProps) {
     return (
-        <div className="flex items-start">
-        <div className="bg-blue-500 text-white p-3 rounded-lg max-w-xs shadow-md">
-          {chat}
-        </div>
-      </div>
+        <>
+        <div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">Delivered</div>
+</div>
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Anakin
+    <time className="text-xs opacity-50">12:46</time>
+  </div>
+  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-footer opacity-50">Seen at 12:46</div>
+</div>
+        </>
     )
 }
 
