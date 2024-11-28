9a3c673ff49215cb1f66cb559273ae4b349d526f
Damian X <dx4336969@gmail.com>
2024-11-28 08:25:05 +0000

    implementing sticky account header nav

--- src/features/dashboard/account/AccountHeader.tsx
+++ src/features/dashboard/account/AccountHeader.tsx
@@ -2,7 +2,7 @@ import { IoIosArrowBack } from "react-icons/io"
 
 function AccountHeader() {
     return (
-        <div>
+        <div className="sticky mb-8  left-0 right-0 top-0 py-2 pb-6  shadow-md bg-white">
             <div className="flex pt-4 items-center justify-between mx-4">
             <IoIosArrowBack className="size-5"/>
             <button className="">Logout</button>
