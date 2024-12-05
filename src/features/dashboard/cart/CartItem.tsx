1fd517b8f2655e7f2cd82668c9dc2e1351d7422c
Damian X <dx4336969@gmail.com>
2024-12-05 11:30:34 +0000

    implementing checkbox /cart

--- src/features/dashboard/cart/CartItem.tsx
+++ src/features/dashboard/cart/CartItem.tsx
@@ -4,7 +4,9 @@ import QuantityNav from "./QuantityNav"
 function CartItem() {
     return (
         <div className="mx-2 " >
-            <div className="grid grid-cols-[1fr_2fr] items-center border rounded-lg">
+            <div className="grid grid-cols-[1fr_2fr] items-center border rounded-lg relative">
+            
+            <input type="checkbox" defaultChecked className="checkbox absolute top-2 left-2 checkbox-warning " />
             <img src="https://i.ibb.co/GdLN0zn/Luxury-watch-with-leather-wrist-band-isolated-on-transparent-background-PNG.png" alt="Luxury-watch-with-leather" />
                 <div>
                     <h1 className="text-xs font-bold font-mulish tracking-tight opacity-60 text-gray-700">Apple Watch Ultra 2 with Ocean Band</h1>
