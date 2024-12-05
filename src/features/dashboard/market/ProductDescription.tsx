313e59625a8c565780b31276c331364b391f051e
Damian X <dx4336969@gmail.com>
2024-12-05 06:47:05 +0000

    implementing product details

---
+++ src/features/dashboard/market/ProductDescription.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { FaRegHeart } from "react-icons/fa6"
+
+function ProductDescription() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function toggleReadMore(){
+        setIsExpanded(!isExpanded);
+    };
+    return (
+        <div className="mx-4 pt-4">
+                <div>
+                    <div className="flex mt-4 justify-between items-center">
+
+                    <h1 className="font-Montserrat font-bold">Ipad Pro 6th Generation 11 inch 2022</h1>
+                    <FaRegHeart/>
+                    </div>
+                    <div className="flex gap-4 items-center pt-2">
+                        <span className="text-lg font-bold">
+                        GH₵15.299
+                        </span>
+                        <p className="border px-3  rounded-full bg-red-100 text-red-600 font-semibold">6% off</p>
+
+                    </div>
+                    <p className="line-through opacity-70">GH₵55.00</p>
+
+                </div>
+
+                <div>
+                    <h1 className="font-bold pt-6 pb-2 ">Product Description</h1>
+                    <p className="text-sm"> {!isExpanded?'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio suscipit maiores placeat perferendis quas debitis voluptas nesciunt optio ipsam quidem, dolor distinctio? Aut, nemo distinctio!....':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore a debitis beatae? Illo possimus, deleniti incidunt quidem voluptas maiores quis magni soluta tempora nesciunt laudantium totam eaque excepturi accusamus debitis perspiciatis sunt veniam eveniet ab facilis mollitia odio aliquid? Enim.'} <span onClick={()=>setIsExpanded(expand=>!expand)} className="text-dasadeep">{isExpanded?'Read less':'Read more'}</span></p>
+                    
+                    
+                </div>
+            </div>
+    )
+}
+
+export default ProductDescription
--- src/features/dashboard/market/ProductDetail.tsx
+++ src/features/dashboard/market/ProductDetail.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom"
 import { Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
 import SliderMain from "../components/SliderMain"
+import { FaRegHeart } from "react-icons/fa6"
+import { useState } from "react"
+import ProductDescription from "./ProductDescription"
+import { FaShoppingCart } from "react-icons/fa"
 
 
 
@@ -13,6 +17,8 @@ function ProductDetail() {
     const navigate = useNavigate()
     const imgs = ["https://i.ibb.co/5T0GmMy/sneaker-2.png","https://i.ibb.co/F7K9fjg/sneaker-3.png","https://i.ibb.co/L5Z1hNM/sneaker-4.png","https://i.ibb.co/PcPBVyC/sneaker-1.jpg"
         ]
+
+      
     return (
         <div>
             <div className="flex items-center justify-between px-3 pt-3">
@@ -26,8 +32,20 @@ function ProductDetail() {
                 <IoShareSocialOutline className="size-5"/>
                 </div>
             </div>
-            <div>
+            <div >
+
+            <div className="mx-8" >
                 <SliderMain el={imgs}/>
+            </div>
+            <ProductDescription/>
+            </div>
+            <div className="flex items-center justify-center absolute bottom-0 left-0 right-0 py-4 gap-4 bg-white ">
+                <div className="flex items-center border border-dasadeep p-3 px-5 rounded-lg text-dasadeep gap-2">
+                <FaShoppingCart/><span> Add to Cart</span>
+                </div>
+                <div className="flex items-center border border-dasadeep bg-dasadeep p-3 px-5 rounded-lg text-white gap-2">
+                    Checkout
+                </div>
             </div>
         </div>
     )
