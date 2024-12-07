e0c72dc6f921b35190a9b8eec21c62502b8ffbe9
Damian X <dx4336969@gmail.com>
2024-12-07 09:24:26 +0000

    implementing orders page /orders

--- src/features/dashboard/order/OrderUser.tsx
+++ src/features/dashboard/order/OrderUser.tsx
@@ -1,11 +1,25 @@
+import { FaBoxOpen } from "react-icons/fa6"
 import ProductNav from "../market/ProductNav"
+import { BiPackage } from "react-icons/bi"
 
 function OrderUser() {
     return (
-        <div>
+        <div className="bg-white">
             <ProductNav type="simple" title="Your Order"/>
+<div className="h-dvh flex justify-center  ">
+    <div className="flex items-center space-y-10 flex-col">
 
-            <p>Your Orders will appear here :)</p>
+
+            
+            <div className="pt-36">
+                <div className="flex flex-col items-center">
+
+                <BiPackage className="size-24"/>
+            <p className="font-semibold text-gray-600">empty</p>
+                </div>
+    </div>
+            </div>
+</div>
         </div>
     )
 }
