79d0b3d433a8d9c37d3c0a057524568e0670188f
Damian X <dx4336969@gmail.com>
2024-12-12 09:58:00 +0000

    implementing image rename /uploadproduct

--- src/features/dashboard/market/DropDownButton.tsx
+++ src/features/dashboard/market/DropDownButton.tsx
@@ -1,32 +1,31 @@
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { toggleRenameImage } from "@/features/slices/navSlice";
 import { useAppDispatch } from "@/features/utils/hooks";
 import { ReactElement } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
-import { BiDotsVerticalRounded } from "react-icons/bi";
 import { LuTextCursorInput } from "react-icons/lu";
-import { MdDeleteForever } from "react-icons/md";
+
 export type dropDownButtonProps = {
   icon: ReactElement;
-  handleRemoveImage: (id: number) => void;
   id: number;
+  handleToggleRename: ()=>void
+  handleRemoveImage: (id: number) => void;
 };
-function DropDownButton({ icon, id, handleRemoveImage }: dropDownButtonProps) {
-  const dispatch = useAppDispatch();
+function DropDownButton({ icon, id, handleRemoveImage,handleToggleRename }: dropDownButtonProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>{icon}</DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Edit Image</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => dispatch(toggleRenameImage())} className="flex justify-between pr-2">
+        <DropdownMenuItem onClick={() => handleToggleRename()} className="flex justify-between pr-2">
           Rename <LuTextCursorInput/>
         </DropdownMenuItem>
         <DropdownMenuItem className="flex justify-between pr-2" onClick={()=>handleRemoveImage(id)}>Delete<AiOutlineDelete/></DropdownMenuItem>
--- src/features/dashboard/market/UploadProductImg.tsx
+++ src/features/dashboard/market/UploadProductImg.tsx
@@ -2,11 +2,11 @@ import { BiDotsVerticalRounded } from "react-icons/bi";
 import { IoMdClose } from "react-icons/io";
 import { extendFile } from "./DragZone";
 
-import DropDownButton from "./DropDownButton";
-import { useAppDispatch, useAppSelector } from "@/features/utils/hooks";
-import { FormEvent, useState } from "react";
 import { toggleRenameImage } from "@/features/slices/navSlice";
+import { useAppDispatch, useAppSelector } from "@/features/utils/hooks";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import DropDownButton from "./DropDownButton";
 export type uploadPrductImgProps = {
   file: extendFile;
   id: number;
@@ -18,17 +18,22 @@ function UploadProductImg({
   file,
   handleRemoveImage,
 }: uploadPrductImgProps) {
-  const {renameImage} = useAppSelector(store=>store.nav)
-  const dispatch = useAppDispatch()
+  
+  const [renameImage, setRenameImage] = useState<boolean|null>()
   const [newImgName, setNewImgName] = useState<string|null>()
-  const {handleSubmit,register} = useForm()
+  const {handleSubmit,register,reset} = useForm()
   function handleRename(data){
+    
     setNewImgName(data?.imgName)
-    dispatch(toggleRenameImage())
+    reset()
+    handleToggleRename()
 
   }
+  function handleToggleRename(){
+    setRenameImage(reveal=>!reveal)
+  }
   return (
-    <div className="relative select-none   ">
+    <div className="relative    ">
       <IoMdClose
         className="absolute  left-0 top-0"
         onClick={() => handleRemoveImage(id)}
@@ -38,18 +43,18 @@ function UploadProductImg({
         alt={file.name}
         className="size-48 object-cover object-center "
       />
-      <form onSubmit={handleSubmit((data)=>handleRename(data))}>
 
-      <p className="text-center text-xs tracking-tight flex justify-between items-center pt-1 font-bold">
+      <form onSubmit={handleSubmit((data)=>handleRename(data))}>
+      <div className="text-center text-xs tracking-tight flex justify-between items-center pt-1 font-bold">
         
         {renameImage?<input type="text" {...register("imgName")} id="" className="bg-white w-full" />:
         newImgName || file.name.split(".")[0].slice(0, 12)
       }
 
-<DropDownButton icon={<BiDotsVerticalRounded className="translate-x-1 size-4"/>} handleRemoveImage={handleRemoveImage} id={id}/>
-      </p>
+<DropDownButton icon={<BiDotsVerticalRounded className="translate-x-1 size-4"/>} handleRemoveImage={handleRemoveImage} handleToggleRename={handleToggleRename} id={id}/>
+      </div>
       {renameImage && <div className="flex pl-1 pt-2">
-         <button  className="bg-dasadeep text-xs px-2 font-poppins font-semibold rounded-sm py-1 ">Rename</button>
+         <button   className="bg-dasadeep text-xs px-2 font-poppins font-semibold rounded-sm py-1 ">Rename</button>
       </div>
          }
          </form>
