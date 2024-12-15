d71213c88f35d318554c689108ab01538332c8c9
Damian X <dx4336969@gmail.com>
2024-12-15 14:48:13 +0000

    implementing change

--- src/features/dashboard/announcement/Announcement.tsx
+++ src/features/dashboard/announcement/Announcement.tsx
@@ -5,13 +5,13 @@ import { FaRegSmileBeam } from "react-icons/fa";
 import { LiaComment } from "react-icons/lia";
 function Announcement() {
     return (
-        <div className="mx-4 border-2 shadow-lg mt-4 pt-2 px-2">
-            <div className="grid grid-cols-[2fr_.5fr] ">
+        <div className="mx-4 border-2 shadow-lg mt-4 pt-2 px-2 rounded-md ">
+            <div className="grid grid-cols-[2fr_.5fr] pt-2 pr-1 ">
                 <div className="flex space-x-2">
                 <img src="https://i.ibb.co/LdfLkxF/photo-89-2024-10-31-06-52-36.jpg" className="size-14 rounded-full"/>
 
                     <div >
-                        <h1 className="font-Montserrat font-semibold pt-2">Damian Nannet</h1>
+                        <h1 className="font-Montserrat font-semibold ">Damian Nannet</h1>
                         <p className="text-sm">Vice President - Mar 16, 09:00pm</p>
 
                     </div>
@@ -33,19 +33,20 @@ function Announcement() {
                 <h1 className="font-bold font-mulish text-lg">Welcome to DaSA humble Dasians</h1>
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias quaerat ratione eveniet id rem. Doloremque ad natus architecto praesentium.</p>
-                <div className="flex justify-between py-2">
-                  <h1>
-                  <span className="text-lg">
+                <div className="flex justify-between py-2 pr-3 items-center">
+                  <h1 className="font-semibold">
+                  <span className="">
                   👍🤯</span>  12 person
                     </h1> 
                     <div>
-                        <p>2 Comments</p> 
+                        <p className="font-semibold">2 Comments</p> 
                         </div> 
                 </div>
                 <div>
-                    <div className="flex gap-4 border-t-2 py-2 pb-3">
-                       <span className="flex items-center gap-2 justify-center"><FaRegSmileBeam/> Reactions</span> 
-                       <span className="flex items-center gap-2 justify-center"><LiaComment/> Comment</span> 
+                    <div className="flex gap-4 border-t-2 py-2 pb-3  ">
+                       <span className="flex items-center gap-2 justify-center 
+                       font-semibold"><FaRegSmileBeam/> Reactions</span> 
+                       <span className="flex items-center gap-2 justify-center font-semibold"><LiaComment/> Comment</span> 
                     </div>
                 </div>
             </div>
--- src/features/dashboard/components/Welcome.tsx
+++ src/features/dashboard/components/Welcome.tsx
@@ -2,18 +2,21 @@ import SVGLitex from "../../ui/SVGLitex"
 
 function Welcome() {
     return (
-        <div className="border-2 mx-4 mt-4 shadow-md">
+        <div className="border-2 mx-4 my-6 shadow-md rounded-md ">
           <div className="px-2 py-2 pb-7">
-            <h1 className=" font-mulish font-bold text-lg">Hey Damian 👋</h1>
-            <p className="font-poppins tracking-wide ">
-            Here's what's happening with your product today
-            </p>
+            <div className="overflow-hidden flex justify-center  ">
+
+          <img className="  w-full" src="https://i.ibb.co/YtfKtdq/photo-79-2024-10-31-06-52-36.jpg" alt="photo-79-2024-10-31-06-52-36" />
+            </div>
+            <h1 className=" font-mulish font-bold text-lg  pt-4">Hey Damian 👋</h1>
+            <p className="font-poppins tracking-wide text-sm py-1 pb-2  ">
+            Here’s everything you need to know about DaSA today!</p>
             <div className="flex items-center justify-center gap-16">
-                <p className="flex flex-col   text-center"><span className="font-bold font-mulish text-lg">
-                83</span>  <span className="leading-5 font-mulish font-medium">Anonymous <br /> Messages</span></p>
+                <p className="flex flex-col   text-center"><span className="font-bold font-Montserrat text-lg">
+                83</span>  <span className="font-poppins font-medium text-sm tracking-wide  leading-4">Anonymous <br /> Messages</span></p>
             
-                <p className="flex flex-col text-center"><span className="font-bold font-mulish text-lg">
-                83</span> <span className="font-mulish leading-5 font-medium ">Anonymous <br /> Messages</span></p>
+                <p className="flex flex-col text-center"><span className="font-bold font-Montserrat text-lg">
+                83</span> <span className="font-poppins font-medium text-sm tracking-wide  leading-4">Anonymous <br /> Messages</span></p>
             </div>
             
           </div>
