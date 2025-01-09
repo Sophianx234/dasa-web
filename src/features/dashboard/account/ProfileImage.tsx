f9555b0f4f11578416497d8278206fe0e35a3963
Damian X <dx4336969@gmail.com>
2025-01-09 06:55:21 +0000

    implementing change user profile

--- src/features/dashboard/account/ProfileImage.tsx
+++ src/features/dashboard/account/ProfileImage.tsx
@@ -1,7 +1,11 @@
+import { useGetUser } from "@/features/utils/hooks";
+import { userType } from "@/services/apiServices";
 import { ChangeEvent, useRef } from "react";
 import { CiEdit } from "react-icons/ci";
 
 function ProfileImage() {
+  const {data} = useGetUser()
+  const {user} = data as userType 
   const fileInputRef = useRef<HTMLInputElement |null>(null);
 
   function handleButtonClick(){
@@ -9,16 +13,19 @@ function ProfileImage() {
   };
 
   function handleFileChange(e: ChangeEvent<HTMLInputElement>){
-    const file = e?.target.files[0];
-    if (file) {
-      console.log("Selected file:", file.name);
+    if(e.target.files && e.target.files.length>0){
+
+      const file = e?.target.files[0];
+      if (file) {
+        console.log("Selected file:", file.name);
+      }
     }
   };
     return (
         <div className="pt-14 pb-16 bg-gray-50   flex items-center justify-center">
         <div className="avatar relative online">
   <div className="w-24  rounded-full overflow-y-auto">
-    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <img src={user.profileImage} />
     <div onClick={handleButtonClick} className="bg-white shadow-md rounded-full absolute translate-x-16 -translate-y-8 px-2 py-2">
 
     <input type="file" className="hidden"
