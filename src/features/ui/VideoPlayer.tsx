6f1d72262b18ca0d3deee4faa2ae6262514e79ed
Damian X <dx4336969@gmail.com>
2025-01-20 08:46:51 +0000

    implementing video play onscroll

--- src/features/ui/VideoPlayer.tsx
+++ src/features/ui/VideoPlayer.tsx
@@ -1,76 +1,161 @@
-import { BsInstagram } from "react-icons/bs"
-import { FaXTwitter } from "react-icons/fa6"
-import { FiFacebook } from "react-icons/fi"
-import { PiTelegramLogo } from "react-icons/pi"
-import ReactionList from "./ReactionList"
-import SVGLite from "./SVGLite"
-import { useEffect, useRef, useState } from "react"
+import { BsInstagram } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
+import { FiFacebook } from "react-icons/fi";
+import { PiTelegramLogo } from "react-icons/pi";
+import ReactionList from "./ReactionList";
+import SVGLite from "./SVGLite";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useIsVisible } from "../utils/useIsVisible";
 
 export type videoPlayerProps = {
-    src: string
-} 
-function VideoPlayer({src}:videoPlayerProps) {
-    const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  src: string;
+};
+function VideoPlayer({ src }: videoPlayerProps) {
+  const { isVisible, targetRef } = useIsVisible(
+    {
+      root: null,
+      rootMargin: "200px",
+      threshold: 0.1,
+    },
+    false
+  );
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const startVideoOnMouseMove = useCallback(async () => {
+    try {
+      await videoRef.current?.play();
+    } catch (e) {
+      // do nothing
+    }
+  }, []);
+
+  const stopVideoOnMove = useCallback(() => {
+    try {
+      videoRef.current?.pause();
+    } catch (e) {
+      // do nothing
+    }
+  }, []);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsVideoLoaded(true); // Load video when in view
-          videoRef.current?.play(); // Play video automatically
-        } else {
-          videoRef.current?.pause(); // Pause video when out of view
-        }
-      },
-      { threshold: 0.5 } // Trigger when 50% of the video is in view
-    );
+    if (isVisible) {
+      startVideoOnMouseMove();
+    } else {
+      stopVideoOnMove();
+    }
+  }, [isVisible, startVideoOnMouseMove, stopVideoOnMove]);
 
-    const videoElement = videoRef.current;
-    if (videoElement) observer.observe(videoElement);
 
-    return () => {
-      if (videoElement) observer.unobserve(videoElement);
-    };
-  }, []);
-    
-    return (
-        
-        <>
-        <div className="grid grid-cols-[.2fr_2fr_.2fr] gap-3">
-            <div className="flex   flex-col items-center  gap-16 pt-2">
-
+  return (
+    <>
+    <span
+      ref={targetRef}
+      
+    >
+      <div className="grid grid-cols-[.2fr_2fr_.2fr] gap-3">
+          <div className="flex   flex-col items-center  gap-16 pt-2">
             <div className="gap-4 flex flex-col items-center   pl-2">
-            <FaXTwitter className="size-5 fill-[#ffd8a8] "/>
-            <FiFacebook className="size-5 stroke-[#ffd8a8]"/>
+              <FaXTwitter className="size-5 fill-[#ffd8a8] " />
+              <FiFacebook className="size-5 stroke-[#ffd8a8]" />
             </div>
             <div className="pb-10 space-y-3">
-            <PiTelegramLogo className="size-5 fill-[#ffd8a8] "/>
-            <BsInstagram className="size-5 fill-[#ffd8a8]"/>
+              <PiTelegramLogo className="size-5 fill-[#ffd8a8] " />
+              <BsInstagram className="size-5 fill-[#ffd8a8]" />
+          </div>
+          </div>
+      <video
+        ref={videoRef}
+        loop
+        muted
+        autoPlay={false}
+        preload="none"
+        playsInline
+        controls
+        className="w-72 pb-4 "
+        
+      >
+        <source src={src}  type="video/mp4" />
+        Your browser does not support the video tag. Please try viewing this
+        page in a modern browser.
+      </video>
+      <div className="flex">
+            <div className="self-end">
+              <ReactionList />
             </div>
-            </div>
+          </div>
+        </div>
+        
+    </span>
+    <div className="pb-12">
 
-            {isVideoLoaded && (
+        <SVGLite type="lines"  />
+    </div>
+    </>
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* return (
+    <span
+      ref={targetRef}
+      
+    >
+      <>
+        <div className="grid grid-cols-[.2fr_2fr_.2fr] gap-3">
+          <div className="flex   flex-col items-center  gap-16 pt-2">
+            <div className="gap-4 flex flex-col items-center   pl-2">
+              <FaXTwitter className="size-5 fill-[#ffd8a8] " />
+              <FiFacebook className="size-5 stroke-[#ffd8a8]" />
+            </div>
+            <div className="pb-10 space-y-3">
+              <PiTelegramLogo className="size-5 fill-[#ffd8a8] " />
+              <BsInstagram className="size-5 fill-[#ffd8a8]" />
+            </div>
+          </div>
+
           <video
             ref={videoRef}
-            className="w-64 pb-10"
+            className="w-64 pb-10 z-50"
             src={src}
             controls
-            muted
             loop
-          ></video>
-        )}
-        
+            muted
+            autoPlay={false}
+            preload="none"
+            playsInline
+            onError={() => console.error("Video failed to load")}
+          />
 
-        <div className="flex">
+          <div className="flex">
             <div className="self-end">
-                <ReactionList/>
+              <ReactionList />
             </div>
+          </div>
         </div>
-        </div>
-        <SVGLite type='lines'/>
-        </>
-    )
+        <SVGLite type="lines" />
+      </>
+    </span>
+  ); */
 }
 
-export default VideoPlayer
+
+export default VideoPlayer;
