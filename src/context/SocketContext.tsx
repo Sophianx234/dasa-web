902d7090962c39dd97cdde2698676d300d3b309d
Damian X <dx4336969@gmail.com>
2025-01-29 06:50:21 +0000

    fix socket io not working

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,55 +1,61 @@
+localStorage.debug = "*";
 
 import { useAppSelector } from '@/features/utils/hooks';
 import { signupCredentialsExtended } from '@/services/apiServices';
-import { createContext, ReactNode, useContext, useEffect, useRef } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 type SocketContextType = Socket | null;
-export const SocketContext = createContext<SocketContextType>(null) 
+const SocketContext = createContext<SocketContextType>(null) 
 type socketProviderProps = {
   children: ReactNode
 }
 
+function useSocket(){
+  const context = useContext(SocketContext)
+  if(context === undefined) throw new Error ('context was called outside of provider')
+    return context ;
+}
 
 function SocketProvider({children}:socketProviderProps) {
-  const socket = useRef<Socket | null>(null);
+  const [socket,setSocket] = useState<Socket | null>(null);
   const {user} = useAppSelector(store=>store.nav)
   const userInfo = user as signupCredentialsExtended
   console.log(userInfo)
   useEffect(() => {
     if (userInfo._id ) {
-      socket.current = io("http://localhost:8000", {
-        withCredentials: true,
+    const socketInstance = io("http://localhost:8000", {
         query: { userId: userInfo._id},
         reconnection: true,
+        withCredentials: true,
         reconnectionAttempts: 5,
         transports: ['websocket']
       });
+      setSocket(socketInstance)
+      console.log("Socket initialized:", socket);
+      
 
       
 
       // socket.current.on("receiveMessage", handleReceiveMessage);
       
       return () => {
-        if(socket.current){
-          socket.current.disconnect();
+        if(socketInstance){
+          
+          socketInstance.disconnect();
 
         }
       };
     }
-  }, [userInfo]);
+  }, []);
     return (
-        <SocketContext.Provider value={socket.current}>
+        <SocketContext.Provider value={socket}>
   {children}           
         </SocketContext.Provider>
     )
 }
 
-function useSocket(){
-  const context = useContext(SocketContext)
-  if(context === undefined) throw new Error ('context was called outside of provider')
-    return context;
-}
 
-export { SocketProvider, useSocket };
+
+export { SocketProvider, useSocket};
 
--- src/features/dashboard/Chat/ChatSendInput.tsx
+++ src/features/dashboard/Chat/ChatSendInput.tsx
@@ -1,24 +1,45 @@
-import { sendMessage } from "@/features/slices/userSlice"
-import { useAppDispatch } from "@/features/utils/hooks"
-import { useForm } from "react-hook-form"
-import { LuSendHorizonal } from "react-icons/lu"
-
+import { useSocket } from "@/context/SocketContext";
+import { sendMessage } from "@/features/slices/userSlice";
+import { useAppDispatch } from "@/features/utils/hooks";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { LuSendHorizonal } from "react-icons/lu";
+import { formValues } from "../account/ChangeContactForm";
+type sendMessageFormValues = formValues & {
+  message: string;
+};
 function ChatSendInput() {
-    const dispatch = useAppDispatch()
-    const {handleSubmit,register,reset} = useForm()
+  const socket = useSocket();
+  const dispatch = useAppDispatch();
+  const { handleSubmit, register, reset } = useForm<sendMessageFormValues>();
+
+  const handleSendAnonymous: SubmitHandler<sendMessageFormValues> = (
+    data: sendMessageFormValues
+  ) => {
+    console.log(data)
+    
+    socket?.emit("sample", { content: data.message }, (response:Response) => {
+      console.log("Server response:", response); 
+    });
+    dispatch(sendMessage(data.message));
+    reset();
+  }
+
     return (
-        <form onSubmit={handleSubmit(data=>{
+      <form
+        onSubmit={handleSubmit(handleSendAnonymous)}
+        className="flex py-3 text-white items-center space-x-2 z-40 justify-center  bg-dasalight"
+      >
+        <input
+          {...register("message")}
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered  max-w-xs"
+        />
+        <button>
+          <LuSendHorizonal className="hover:stroke-dasadeep duration-150 transition-all size-9 stroke-slate-900 " />
+        </button>
+      </form>
+    );
+  };
 
-            dispatch(sendMessage(data.text))
-            reset()
-            })} className="flex py-3 text-white items-center space-x-2 z-40 justify-center  bg-dasalight">
-              <input {...register('text')} type="text" placeholder="Type here" className="input input-bordered  max-w-xs" />
-              <button>
-      
-              <LuSendHorizonal  className="hover:stroke-dasadeep duration-150 transition-all size-9 stroke-slate-900 "/>
-              </button>
-              </form>
-    )
-}
-
-export default ChatSendInput
+export default ChatSendInput;
