bdc1dfb0a1afece8bbee66dadc6b9ada0c3c4db4
Damian X <dx4336969@gmail.com>
2025-03-15 11:05:08 +0000

    add styles to market bottom navlinks

--- src/features/dashboard/market/BottomNavItem.tsx
+++ src/features/dashboard/market/BottomNavItem.tsx
@@ -14,13 +14,13 @@ function BottomNavItem({link,icon,title}:bottomNavItem) {
    
     if(title.toLowerCase() !== 'add')
     return (
-        <Link to={link} className="flex flex-col justify-center items-center group">
+        <Link to={link} className="flex flex-col justify-center items-center group pb-2 border-b-[3px]  px-2 border-transparent hover:border-dasadeep rounded-sm duration-200 ease-out">
                 <span>{icon}</span>
                 <span className="font-Montserrat font-semibold">{title}</span>
             </Link>
     )
     else return (
-    <div className="flex flex-col justify-center items-center group"
+    <div className="flex flex-col justify-center items-center group pb-2 border-b-[3px]  px-2 border-transparent hover:border-dasadeep rounded-sm transition-all duration-200 ease-in-out"
     onClick={()=>dispatch(toggleRevealUploadProduct())}>
     <span>{icon}</span>
     <span className="font-Montserrat font-semibold">{title}</span>
