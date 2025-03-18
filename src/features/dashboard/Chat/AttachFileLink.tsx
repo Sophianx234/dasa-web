6e26624277cffe44fb939f2e6eca5eb8130342a1
Damian X <dx4336969@gmail.com>
2025-03-18 14:41:03 +0000

    adding upload images in anonymous

--- src/features/dashboard/Chat/AttachFileLink.tsx
+++ src/features/dashboard/Chat/AttachFileLink.tsx
@@ -1,20 +1,35 @@
 import { setIsOpenAttachFile } from "@/features/slices/navSlice";
 import { useAppDispatch } from "@/features/utils/hooks";
-import { ReactNode, useRef } from "react";
+import { ChangeEvent, ReactNode, useRef } from "react";
+import { useForm } from "react-hook-form";
+import { formValues } from "../account/ChangeContactForm";
+import axios from "axios";
+import { API_URL } from "@/services/apiServices";
 type attachFileLinkProps = {
   icon: ReactNode;
   text: string;
   accept: string;
 };
+type uploadAttachedFileFormInput = formValues &{
+  img:string
+}
 function AttachFileLink({ icon, text, accept }: attachFileLinkProps) {
   const dispatch = useAppDispatch()
   const fileRef = useRef<HTMLInputElement | null>(null);
-  function handleClick() {
+
+   function handleClick() {
     if (fileRef.current) {
       fileRef.current.click();
     }
   }
-  function handleFileUpload(){
+  async function handleFileUpload(e:ChangeEvent<HTMLInputElement>){
+    const formData = new FormData()
+    for(const i in  e.target.files){
+      const file = e.target.files?.[+i]
+      formData.append('img',file)
+      await axios.post(`${API_URL}/messages/anonymous/upload`,formData)
+
+    }
 
     dispatch(setIsOpenAttachFile(false))
 
@@ -27,9 +42,10 @@ function AttachFileLink({ icon, text, accept }: attachFileLinkProps) {
       >
         {icon} <span className="font-semibold text-sm ">{text}</span>
       </li>
-      <input type="file" className="hidden" ref={fileRef}
+      <input type="file"  className="hidden" ref={fileRef}
       accept={accept}
-      onChange={handleFileUpload} />
+      onChange={handleFileUpload}
+       />
     </>
   );
 }
