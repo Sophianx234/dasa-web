06b3b4c9408df42610b8b09cf6b5c11073f64a33
Damian X <dx4336969@gmail.com>
2025-05-06 23:28:04 +0000

    fix: broken constitution download

--- src/features/ui/ConstitutionSection.tsx
+++ src/features/ui/ConstitutionSection.tsx
@@ -15,7 +15,7 @@ function ConstitutionSection() {
         />
         <div className=" w-full text-center  ">
           <a
-            href="/DRAFTED CONSTITUTION - REVIEW - DAGOMBA STUDENTS-1.pdf"
+            href="/DRAFTED_CONSTITUTION.pdf"
             className="bg-dasadeep  h-full py-2 font-bold text-lg     border hover:scale-100 border-dasadeep 
              transition-all duration-300 flex items-center justify-center gap-1"
           >
