6176901377b5ed1745f026f97faee6c34c127a05
Damian X <dx4336969@gmail.com>
2025-05-07 12:04:47 +0000

    fix: download error

--- src/features/ui/ConstitutionSection.tsx
+++ src/features/ui/ConstitutionSection.tsx
@@ -15,8 +15,8 @@ function ConstitutionSection() {
         />
         <div className=" w-full text-center  ">
           <a
-          download
-            href="/DRAFTED_CONSTITUTION.pdf"
+          
+            href="/drafted_constitution.pdf"
             className="bg-dasadeep  h-full py-2 font-bold text-lg     border hover:scale-100 border-dasadeep 
              transition-all duration-300 flex items-center justify-center gap-1"
           >
