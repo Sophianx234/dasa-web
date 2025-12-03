15db6b931cdaac59ccb26894224ab47c591df99b
sophianx234 <dx4336969@gmail.com>
2025-12-03 22:02:14 +0000

    feat: add dropdown nav to gallery page

--- src/features/pages/Gallerypage.tsx
+++ src/features/pages/Gallerypage.tsx
@@ -1,11 +1,17 @@
+import { AnimatePresence } from "framer-motion";
 import BriefGallery from "../dashboard/components/BriefGallery"
 import Footer from "../ui/Footer"
 import Header from "../ui/Header"
+import NavLinks from "../ui/NavLinks";
+import { useAppSelector } from "../utils/hooks";
 
 function Gallerypage() {
+    const isOpen = useAppSelector((store) => store.nav.isOpen);
+  
   return (
     <div className="h-dvh  scrollbar-hide overflow-x-hidden w-dvw">
       <Header />
+      <AnimatePresence>{isOpen && <NavLinks swap="col" />}</AnimatePresence>
       <div className="pt-32 ">
 
         <BriefGallery style="side"/>
